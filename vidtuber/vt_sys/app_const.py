052302c575f342cc3eeb764f9b80f22c22882822
Gianluca Pernigotto <jeanlucperni@gmail.com>
2025-08-28 17:30:39 +0200

    Fix supported langs

--- vidtuber/vt_sys/app_const.py
+++ vidtuber/vt_sys/app_const.py
@@ -20,20 +20,20 @@ langDomain = "vidtuber"
 # `int` objects, e.g wx.LANGUAGE_CZECH is 150 and wx.LANGUAGE_CZECH_CZECHIA
 # is 151, ecc.
 
-
 supLang = {  # languages you want to support.
     "Default": (wx.LANGUAGE_DEFAULT, ("System default language")),
     "ar_SA": (wx.LANGUAGE_ARABIC_SAUDI_ARABIA, ("Arabic (Saudi Arabia)")),
     "zh_CN": (wx.LANGUAGE_CHINESE_SIMPLIFIED, ("Chinese (simplified)")),
-    "cs_CZ": (wx.LANGUAGE_CZECH, ("Czech (Czechia)")),
+    "cs_CZ": (wx.LANGUAGE_CZECH_CZECHIA, ("Czech (Czechia)")),
     "nl_NL": (wx.LANGUAGE_DUTCH, ("Dutch")),
     "en_US": (wx.LANGUAGE_ENGLISH_US, ("English (United States)")),
     "fr_FR": (wx.LANGUAGE_FRENCH, ("French")),
     "de_DE": (wx.LANGUAGE_GERMAN, ("German (Germany)")),
-    "hu_HU": (wx.LANGUAGE_HUNGARIAN, ("Hungarian (Hungary)")),
+    "hu_HU": (wx.LANGUAGE_HUNGARIAN_HUNGARY, ("Hungarian (Hungary)")),
     "it_IT": (wx.LANGUAGE_ITALIAN, ("Italian")),
-    "pt_BR": (wx.LANGUAGE_PORTUGUESE_BRAZILIAN, ("Portuguese (Brazilian)")),
+    "pt_BR": (wx.LANGUAGE_PORTUGUESE_BRAZILIAN,
+                ("Portuguese (Brazilian)")),
     "ru_RU": (wx.LANGUAGE_RUSSIAN, ("Russian")),
     "es_ES": (wx.LANGUAGE_SPANISH, ("Spanish (Spain)")),
     "es_MX": (wx.LANGUAGE_SPANISH_MEXICAN, ("Spanish (Mexico)")),
-    "es_CU": (wx.LANGUAGE_SPANISH, ("Spanish (Cuba)")), }
+    "es_CU": (wx.LANGUAGE_SPANISH_CUBA, ("Spanish (Cuba)")), }
